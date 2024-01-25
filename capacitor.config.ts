eb9199d37dc512f3c2a70b3b7d4dab62208ff078
nabil alsaadi <nabil@buddyboss.com>
2024-01-26 00:41:43 +0400

    fix capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,8 +6,8 @@ const config: CapacitorConfig = {
   webDir: 'build',
   server: {
     androidScheme: 'https',
-    url: 'http://192.168.1.14:3000/',
-    cleartext: true
+    // url: 'http://192.168.1.14:3000/',
+    // cleartext: true
   },
   ios: {
     contentInset: 'always'
